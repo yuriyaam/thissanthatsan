e7f47f74f896e3574101946fc66c55cd873883d1
yuriyaam <hanma56@gmail.com>
2021-04-25 18:44:14 +0900

    최초 파일

---
+++ sansan/src/test/java/com/sansan/sansan/SansanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sansan.sansan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SansanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
